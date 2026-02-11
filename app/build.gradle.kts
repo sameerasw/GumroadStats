60e5d86adb8607afbe272f8a71a96d6db8d6a800
sameerasw <sameerasw.com@gmail.com>
2026-02-11 13:41:22 +0530

    version up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.sameerasw.gumroadstats"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
